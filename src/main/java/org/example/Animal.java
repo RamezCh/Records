3048e84a94a510fe98538eff389a94dc7f3250dc
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-10 11:20:22 +0100

    add: Wither Methods to Animal record

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public record Animal(String ID, String name, Species species, int age, Owner owner) {
+    public Animal withUpdatedID(String ID) {
+        return new Animal(ID, name, species, age, owner);
+    }
+    public Animal withUpdatedName(String name) {
+        return new Animal(ID, name, species, age, owner);
+    }
+    public Animal withUpdatedSpecies(Species species) {
+        return new Animal(ID, name, species, age, owner);
+    }
+    public Animal withUpdatedAge(int age) {
+        return new Animal(ID, name, species, age, owner);
+    }
+    public Animal withUpdatedOwner(Owner owner) {
+        return new Animal(ID, name, species, age, owner);
+    }
 }
